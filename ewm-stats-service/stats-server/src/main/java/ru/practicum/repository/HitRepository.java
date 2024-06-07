a7ab3fb3f6722a3dd7950c34cb4980f9893f6cc0
Egor <alexanoffegor@yandex.ru>
2024-06-07 21:32:51 +0300

    1ST

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ru.practicum.ViewStatsDto;
 import ru.practicum.model.Hit;
 
