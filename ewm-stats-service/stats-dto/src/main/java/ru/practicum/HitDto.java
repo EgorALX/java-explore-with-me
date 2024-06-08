20d4c63620fa61b57e3ed52aafe07ab01a0a5f92
Egor <alexanoffegor@yandex.ru>
2024-06-08 19:30:29 +0300

    test

--- ewm-stats-service/stats-dto/src/main/java/ru/practicum/HitDto.java
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/HitDto.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class HitDto {
     private Long id;
+    @NotBlank
     private String app;
+    @NotBlank
     private String uri;
+    @NotBlank
     private String ip;
+
     private String timestamp;
 }
