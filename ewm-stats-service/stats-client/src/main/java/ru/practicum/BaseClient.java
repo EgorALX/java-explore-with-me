753f2c5534d172ef6d51251416e9e9a4a26ec313
Egor <alexanoffegor@yandex.ru>
2024-06-08 18:36:25 +0300

    test

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/BaseClient.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/BaseClient.java
@@ -1,6 +1,5 @@
 package ru.practicum;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
