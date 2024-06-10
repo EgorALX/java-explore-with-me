5910e9a75b57a532536e80a738a38640d830272e
Egor <alexanoffegor@yandex.ru>
2024-06-11 01:38:19 +0300

    tests

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/mapper/Mapper.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/mapper/Mapper.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Component;
 import ru.practicum.HitDto;
 import ru.practicum.model.Hit;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 @Component
 public class Mapper {
     public Hit toHit(HitDto hitDto) {
