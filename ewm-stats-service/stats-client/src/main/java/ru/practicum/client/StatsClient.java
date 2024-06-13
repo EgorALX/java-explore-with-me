eb4003cf64ebc0ac5802c599f3e07a0820ffec73
Egor <alexanoffegor@yandex.ru>
2024-06-13 22:42:09 +0300

    add edits

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -1,26 +1,28 @@
 package ru.practicum.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.HitDto;
 import ru.practicum.ViewStatsDto;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+@Slf4j
 @Component
 public class StatsClient implements Client {
 
     private static final String HIT_ENDPOINT = "/hit";
+
     private static final String STATS_ENDPOINT = "/stats";
 
     private final RestTemplate rest;
@@ -35,33 +37,41 @@ public class StatsClient implements Client {
     }
 
     public void addHit(HitDto hitDto) {
-        post(hitDto);
+        HttpEntity<HitDto> requestEntity = new HttpEntity<>(hitDto, defaultHeaders());
+        try {
+            ResponseEntity<Object> response = rest.exchange(HIT_ENDPOINT, HttpMethod.POST, requestEntity, Object.class);
+
+            if (response.getStatusCode().is2xxSuccessful()) {
+                log.info("Hit added successfully");
+            } else {
+                log.error("Failed to add hit, status code: {}", response.getStatusCode());
+            }
+        } catch (Exception e) {
+            log.error("Error during adding hit: {}", e.getMessage());
+        }
     }
 
     public List<ViewStatsDto> retrieveAllStats(String start, String end, List<String> uris, boolean unique) {
-        String urisListParam = String.join("&uris=", uris);
-        String path = String.format("%s?start=%s&end=%s&uris=%s&unique=%b",
-                STATS_ENDPOINT, start, end, urisListParam, unique);
-        return get(path);
-    }
-
-    private List<ViewStatsDto> get(String path) {
-        ResponseEntity<Object> response = makeAndSendRequest(HttpMethod.GET, path, null);
-        List<ViewStatsDto> dtos = (List<ViewStatsDto>) response.getBody();
-        return dtos;
-    }
-
-    private <T> void post(T body) {
-        makeAndSendRequest(HttpMethod.POST, HIT_ENDPOINT, body);
-    }
-
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromPath(STATS_ENDPOINT)
+                .queryParam("start", start)
+                .queryParam("end", end)
+                .queryParam("uris", uris)
+                .queryParam("unique", unique);
         try {
-            ResponseEntity<Object> statServerResponse = rest.exchange(path, method, requestEntity, Object.class);
-            return prepareClientResponse(statServerResponse);
-        } catch (HttpStatusCodeException e) {
-            throw new RuntimeException("Failed to send request to stats server", e);
+            ResponseEntity<List<ViewStatsDto>> response = rest.exchange(
+                    uriComponentsBuilder.build().toString(), HttpMethod.GET, null, new ParameterizedTypeReference<>() {}
+            );
+            if (response.getStatusCode().is2xxSuccessful()) {
+                log.info("Successfully retrieved stats");
+                return response.getBody();
+            } else {
+                log.error("HTTP request failed with status code: {}", response.getStatusCode());
+                return List.of();
+            }
+        } catch (Exception e) {
+            log.error("Error: message:{}", e.getMessage());
+            return List.of();
         }
     }
 
@@ -70,12 +80,5 @@ public class StatsClient implements Client {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         return headers;
-    }
-
-    private static ResponseEntity<Object> prepareClientResponse(ResponseEntity<Object> response) {
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response;
-        }
-        return ResponseEntity.ok(new ArrayList<>());
     }
 }
